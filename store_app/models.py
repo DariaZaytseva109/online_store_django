4cb06df0c4dc1a2b4366e4420135960de1f0b975
DariaZaytseva109 <dawazai@mail.ru>
2023-11-02 09:57:52 +0100

    + модели, миграции

--- store_app/models.py
+++ store_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    slug = models.SlugField(max_length=15, unique=True)
+    # image = None
+    price = models.IntegerField()
+    subcategory = models.ForeignKey('Subcategory', null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    slug = models.SlugField(max_length=15, unique=True)
+    # image = None
+    category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    slug = models.SlugField(max_length=15, unique=True)
+    # image = None
+
+    def __str__(self):
+        return self.name
